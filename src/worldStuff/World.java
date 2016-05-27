11d500ae9b181f8786be97c76c180e4e38820442
jnickg <jnickg83@gmail.com>
2016-05-26 20:38:54 -0700

    Flesh out basic world stuff

--- src/worldStuff/World.java
+++ src/worldStuff/World.java
@@ -1,9 +1,13 @@
 package worldStuff;
 
 public class World {
-
+	public static final int DefaultWidth = 1000;
+	public static final int DefaultHeight = 1000;
+	
+	Region[][] _regions;
+	
 	public World() {
-		// TODO Auto-generated constructor stub
+		this._regions = new Region[DefaultHeight][DefaultWidth];
 	}
 
 }
